cc0dbbd7ac2446fdbcb582a867a38b679724a64c
Mohit29061999 <68787888+Mohit29061999@users.noreply.github.com>
2025-02-23 22:34:36 +0530

    Add files via upload

---
+++ EurekaServerDemo/src/test/java/com/gfgDemo/EurekaServerDemo/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfgDemo.EurekaServerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
